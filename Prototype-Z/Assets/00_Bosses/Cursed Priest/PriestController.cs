2edd3a3666b34cae0a38efd4a6d1b75e8039c6f8
queby <nathanyaqueby21@gmail.com>
2023-11-26 14:26:43 +0100

    added attack animations for priest

--- Prototype-Z/Assets/00_Bosses/Cursed Priest/PriestController.cs
+++ Prototype-Z/Assets/00_Bosses/Cursed Priest/PriestController.cs
@@ -4,6 +4,10 @@ public class PriestController : MonoBehaviour
 {
     private Animator animator;
     private bool isAlive = true;
+    private float attackTimer = 0f;
+    private float attackCooldown = 2f; // 2 seconds for attack cooldown
+
+    public Transform player; // Assign this to your player in the Inspector
 
     public float health = 100f;
     // You can add other properties here, like a reference to the player character if needed
@@ -20,23 +24,37 @@ public class PriestController : MonoBehaviour
 
         if (!isAlive) return;
 
-        // Example conditions for different actions
-        bool shouldWalk = false;
-        bool performAttack1 = false;
-        bool performAttack2 = false;
+        // Turn towards the player continuously
+        TurnTowardsPlayer();
 
-        // Set Animator parameters based on the conditions
-        animator.SetBool("isWalking", shouldWalk);
-        if (performAttack1)
+        // Handle attack timing
+        if (attackTimer <= 0f)
         {
-            animator.SetTrigger("attack1");
-            // TODO: Add code to deal damage to the player.
+            PerformAttack();
+            attackTimer = attackCooldown; // Reset attack timer
         }
-        if (performAttack2)
+        else
         {
-            animator.SetTrigger("attack2");
-            // TODO: Add code to deal damage to the player.
+            attackTimer -= Time.deltaTime; // Countdown to next attack
         }
+
+        // Example conditions for different actions
+        // bool shouldWalk = false;
+        // bool performAttack1 = false;
+        // bool performAttack2 = false;
+
+        // // Set Animator parameters based on the conditions
+        // animator.SetBool("isWalking", shouldWalk);
+        // if (performAttack1)
+        // {
+        //     animator.SetTrigger("attack1");
+        //     // TODO: Add code to deal damage to the player.
+        // }
+        // if (performAttack2)
+        // {
+        //     animator.SetTrigger("attack2");
+        //     // TODO: Add code to deal damage to the player.
+        // }
     }
 
     public void TakeDamage(float damage)
@@ -59,5 +77,22 @@ public class PriestController : MonoBehaviour
         // Destroy the object after 2 seconds
         Destroy(gameObject, 2f);
         // TODO: Add code to drop loot, play sound effects, etc.
+    }
+
+    private void TurnTowardsPlayer()
+    {
+        if (player != null)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        }
+    }
+
+    private void PerformAttack()
+    {
+        // Decide between attack1 and attack2 here, for now we'll just trigger attack1
+        animator.SetTrigger("attack1");
+        // TODO: Add code to deal damage to the player.
     }
 }
